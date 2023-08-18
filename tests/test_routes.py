98e06af1c355fa1756893697571bb9a0b14008ac
mystique4u <128178339+mystique4u@users.noreply.github.com>
2023-08-18 12:40:40 +0200

    Update test_routes.py
    syntax fixed

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -135,7 +135,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
-        
+
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
@@ -148,7 +148,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
-    
+
     def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
